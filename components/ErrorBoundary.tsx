050a249dc74e42d4a9c0c4617e9073f174ae29e4
apexformfit-5920 <apexformfit-5920@users.noreply.github.com>
2025-11-15 17:22:06 -0600

    Fix ErrorBoundary default export syntax

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -12,7 +12,7 @@ interface ErrorBoundaryProps {
   fallback?: React.ReactNode
 }
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props)
     this.state = { hasError: false, error: null }
